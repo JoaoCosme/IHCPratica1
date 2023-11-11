dfbd4a9352779ea6d04f17c32b0d54756e2bb5e2
JoaoCosme <cosmejoaopedro@protonmail.com>
2023-11-11 16:13:32 -0300

    sum is now working

--- androidApp/src/main/java/com/ihcpratica1/android/MainActivity.kt
+++ androidApp/src/main/java/com/ihcpratica1/android/MainActivity.kt
@@ -4,18 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,8 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Text(text = "Atividade 1")
-
+                    SumNumbers()
                 }
             }
         }
@@ -35,22 +41,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(phrases: List<String>) {
-    LazyColumn(
-        contentPadding = PaddingValues(20.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        ) {
-        items(phrases){ text ->
-            Text(text)
-            Divider()
+fun SumNumbers(){
+    var num1 by remember { mutableStateOf("0") }
+    var num2 by remember { mutableStateOf("0") }
+    var sumResult by remember { mutableStateOf("0") }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(text = "Atividade 1", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        TextField(value = num1, onValueChange = {num1 = it}, label = { Text(text = "Numero 1")})
+        TextField(value = num2, onValueChange = {num2 = it}, label = { Text(text = "Numero 2")})
+        Button(onClick = {sumResult = (num1.toInt() + num2.toInt()).toString()}) {
+            Text("Somar!")
         }
+        Text(text = sumResult,
+            modifier = Modifier.padding(top = 16.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold)
     }
 }
-
 @Preview
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView(listOf("Hello, Android!"))
-    }
+        SumNumbers()
 }
